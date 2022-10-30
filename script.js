b0b1b37b24bcddb4a93c16bcb1381d92e749c4f5
zantimer <karol.nikiel1@gmail.com>
2022-10-30 17:47:26 +0000

    styled layout, started work on final element: rainbow mode

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const makeGridBtn = document.querySelector('#gridMaker');
 const gridSizeBtn = document.querySelector('#gridCount');
+const wipeBtn = document.querySelector('#wipe');
+const eraserBtn = document.querySelector('#eraser');
+const drawBlackBtn = document.querySelector('#black');
+const rainbowBtn = document.querySelector('#rainbow');
+
 const gridBox = document.querySelector('.gridBox');
 const body = document.querySelector('body');
 let currentGridSize = 0;
 let htmlStyles = window.getComputedStyle(document.querySelector('html'));
 let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
 let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+let currentColor = 'black';
 
 function makeGrid ()
 {
@@ -17,11 +23,15 @@ function makeGrid ()
     {
         const divs = document.createElement('div');
         divs.classList.add('grid');
-        divs.addEventListener('mouseover', changeColor);
+        divs.addEventListener('mouseover', () =>{
+            //divs.classList.add('active');
+            divs.style.backgroundColor = currentColor.toString();
+            currentColor = 'black';
+        });
         gridBox.appendChild(divs);
-        
-    }
-    }
+    };
+    };
+    
 }
 function getGridCount () {
     if (gridBox.childElementCount > 0)
@@ -33,9 +43,9 @@ function getGridCount () {
         
     }
     let b = prompt('Provide grid count, max 100');
-    if (b > 100)
+    if (b > 10)
     {
-        b=100;
+        b=10;
     }
     let result = b*b;
     
@@ -49,10 +59,67 @@ function LightenDarkenColor(col, amt) {
     col = parseInt(col, 16);
     return (((col & 0x0000FF) + amt) | ((((col >> 8) & 0x00FF) + amt) << 8) | (((col >> 16) + amt) << 16)).toString(16);
   };
-function changeColor (color)
+function wipeGrid ()
 {
-    
+    const gridCells = document.getElementsByClassName('grid');
+    for (let i = 0; i < gridCells.length; i++)
+    {
+        gridCells[i].style.backgroundColor = 'white';
+    };
+};
+function eraser() 
+{
+    const gridCells = document.getElementsByClassName('grid');
+    for (let i = 0; i < gridCells.length; i++)
+    {
+        gridCells[i].removeEventListener('mouseover', () =>{
+            //divs.classList.add('active');
+            gridCells[i].style.backgroundColor = currentColor.toString();
+        });
+        gridCells[i].addEventListener('mouseover', () => {
+            currentColor = 'white';
+            gridCells[i].style.backgroundColor = currentColor.toString();
+            
+        });
+    }
+};
+function drawBlack()
+{
+    const gridCells = document.getElementsByClassName('grid');
+    for (let i = 0; i < gridCells.length; i++)
+    {
+        gridCells[i].removeEventListener('mouseover', () =>{
+            //divs.classList.add('active');
+            gridCells[i].style.backgroundColor = currentColor.toString();
+        });
+        gridCells[i].addEventListener('mouseover', () => {
+            currentColor = 'black';
+            gridCells[i].style.backgroundColor = currentColor.toString();
+        });
+    }
+};
+function rainbowMode ()
+{
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const gridCells = document.getElementsByClassName('grid');
+    for (let i = 0; i < gridCells.length; i++)
+    {
+        gridCells[i].removeEventListener('mouseover', () =>{
+            gridCells[i].style.backgroundColor = currentColor.toString();
+        })
+        gridCells[i].addEventListener('mouseover', () => {
+            currentColor = randomColor;
+            gridCells[i].style.backgroundColor = currentColor.toString();
+            
+        });
+    }
 }
+let gridCells = document.getElementsByClassName('grid');
+
+eraserBtn.addEventListener('click', eraser);
+drawBlackBtn.addEventListener('click', drawBlack);
+wipeBtn.addEventListener('click', wipeGrid);
+rainbowBtn.addEventListener('click', rainbowMode);
 gridSizeBtn.addEventListener('click', getGridCount);
 makeGridBtn.addEventListener('click', makeGrid);
 
