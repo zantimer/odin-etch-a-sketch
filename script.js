7690a81201e99bf71fc940be8edfec2fb5e89eb7
zantimer <karol.nikiel1@gmail.com>
2022-11-01 14:04:02 +0000

    removed not working shadow, future TODO

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //query const vars
-const makeGridBtn = document.querySelector('#gridMaker');
 const gridSizeBtn = document.querySelector('#gridCount');
 const wipeBtn = document.querySelector('#wipe');
 const eraserBtn = document.querySelector('#eraser');
@@ -38,8 +37,6 @@ function makeGrid (n = 100)
     };
     };
     addBoolListener();
-    
-    
 }
 function getGridCount () {
     if (gridBox.childElementCount > 0)
@@ -80,7 +77,6 @@ function eraser()
     for (let i = 0; i < gridCells.length; i++)
     {
         gridCells[i].removeEventListener('mouseover', () =>{
-            //divs.classList.add('active');
             gridCells[i].style.backgroundColor = currentColor.toString();
         });
         gridCells[i].addEventListener('mouseover', () => {
@@ -120,35 +116,6 @@ function rainbowMode ()
 };
 
 //sub functions
-// TODO: can't get this one to work - need to revisit when sanity recouperates
-function LightenDarkenColor(col, amt) {
-  
-    var usePound = false;
-  
-    if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
-    }
- 
-    var num = parseInt(col,16);
- 
-    var r = (num >> 16) + amt;
- 
-    if (r > 255) r = 255;
-    else if  (r < 0) r = 0;
- 
-    var b = ((num >> 8) & 0x00FF) + amt;
- 
-    if (b > 255) b = 255;
-    else if  (b < 0) b = 0;
- 
-    var g = (num & 0x0000FF) + amt;
- 
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
- 
-    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-};
 
 function addEvListener (col)
 {
